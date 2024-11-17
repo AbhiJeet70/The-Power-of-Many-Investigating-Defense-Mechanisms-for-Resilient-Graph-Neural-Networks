39421e31efbd3db04b85aadfeac82e7aecce5b70
Abhijeet Dhali <72669714+AbhiJeet70@users.noreply.github.com>
2024-11-16 19:49:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ def run_all_attacks():
                 data_poisoned = attack_fn(data, poisoned_nodes, trigger_gen, alpha=0.7)
             elif attack_name == 'SBA-Gen':
                 data_poisoned = attack_fn(data, poisoned_nodes, trigger_size=5, trigger_density=0.5, model_type='SW')
+            elif attack_name == 'UGBA'
+                data_poisoned = attack_fn(data, num_poisoned_nodes, cluster_threshold=0.8, trigger_density=0.5)
             else:
                 data_poisoned = attack_fn(data, poisoned_nodes)
 
