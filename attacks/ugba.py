1e8556610c3d63f728f4797494843015c9eca6a6
Abhijeet Dhali <72669714+AbhiJeet70@users.noreply.github.com>
2025-06-25 20:35:32 -0400

    Update ugba.py

--- attacks/ugba.py
+++ attacks/ugba.py
@@ -1,6 +1,18 @@
 import torch
 from attacks.trigger_generator import TriggerGenerator
-from gnn_models.encoders import GCNEncoder
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+class GCNEncoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels)
+        self.conv2 = GCNConv(2 * out_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return x
 
 def select_diverse_nodes(data, num_nodes_to_select, num_clusters=None):
     """
